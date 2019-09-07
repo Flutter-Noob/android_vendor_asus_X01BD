5afe2a3ba9f96aa5fb70f21e3d819783fc8ff0ad
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-07 12:01:31 +0530

    X01BD: Move WfdCommon to soong
    Change-Id: I4cdda6d691315aff81a90e73c2ef22a304bee931

--- Android.mk
+++ Android.mk
@@ -276,16 +276,6 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := WfdCommon
-LOCAL_MODULE_OWNER := asus
-LOCAL_SRC_FILES := proprietary/framework/WfdCommon.jar
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := qcrilhook
 LOCAL_MODULE_OWNER := asus
 LOCAL_SRC_FILES := proprietary/framework/qcrilhook.jar
